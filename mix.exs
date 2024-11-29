22b6febfb8b3927be77c421eec33f6608bf0de6f
Serov_Daniil <dserov192@gmail.com>
2024-11-30 01:05:32 +0300

    Update mix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cogito.MixProject do
   def project do
     [
       app: :cogito,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: description(),
